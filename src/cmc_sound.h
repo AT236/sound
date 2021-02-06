861ccacac56016a328a583ac9e15860f1e6e2fc2
AT236 <A_Tews@hotmail.de>
2021-02-06 11:16:39 +0100

    added source files

---
+++ src/cmc_sound.h
@@ -0,0 +1,5 @@
+#ifndef CMC_SOUND_H__
+#define CMC_SOUND_H__
+
+
+#endif // CMC_SOUND_H__
---
+++ src/cmc_sound_hal_api.h
@@ -0,0 +1,5 @@
+#ifndef CMC_SOUND_HAL_API_H__
+#define CMC_SOUND_HAL_API_H__
+
+
+#endif // CMC_SOUND_HAL_API_H__
